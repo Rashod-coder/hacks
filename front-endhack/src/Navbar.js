64a40012640ea8dafbaf43608f78f7763d1402d4
Rishit Gupta <57046026+Rashod-coder@users.noreply.github.com>
2024-04-05 21:58:45 -0700

    changes made

--- front-endhack/src/Navbar.js
+++ front-endhack/src/Navbar.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import {auth} from './auth/Authentication';
+import { auth } from './auth/Authentication';
 import { useState, useEffect } from "react";
-import { onAuthStateChanged, signOut} from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 
 function Navbar() {
   const [user, setUser] = useState("");
-  const [image, setImage] = useState("./images/b lankpfp.png");
+  const [image, setImage] = useState("./images/blankpfp.png");
   const [isUser, setIsUser] = useState(false);
+  
   useEffect(() => {
     onAuthStateChanged(auth, (currentUser) => {
-      if (currentUser) setIsUser(true);
-      if (currentUser && currentUser.displayName)
-        setUser(currentUser?.uid + currentUser.displayName);
-      //we can also set up an image url
-      if (currentUser && currentUser.photoURL) {
-        setImage(currentUser.photoURL);
+      if (currentUser) {
+        setIsUser(true);
+        setUser(currentUser?.displayName); // Set user's display name
+        if (currentUser.photoURL) {
+          setImage(currentUser.photoURL);
+        } else {
+          setImage("./images/blankpfp.png");
+        }
       } else {
-        setUser("");
         setIsUser(false);
+        setUser("");
         setImage("./images/blankpfp.png");
       }
     });
@@ -26,18 +29,17 @@ function Navbar() {
   const logout = async () => {
     try {
       await signOut(auth);
-      // set
       console.log("Signed out");
       setIsUser(false);
     } catch (error) {
-      console.log("error")
+      console.log("error");
     }
   };
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark fixed-top" >
       <div className="container fs-5">
-        <a className="navbar-brand " href="/">Fresh For All</a>
+        <a className="navbar-brand" href="/">Fresh For All</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -59,22 +61,21 @@ function Navbar() {
               </li>
               <li className="nav-item dropdown">
                 <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Account
+                  {isUser ? user : "Account"} {/* Display user's name if logged in, otherwise display "Account" */}
                 </a>
                 <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="/Login">Login</a></li>
-                  <li><a className="dropdown-item" href="/Register">Register</a></li>
-                  <li><a className="dropdown-item" href="/Settings">Settings</a></li>
+                  {!isUser && (
+                    <React.Fragment>
+                      <li><a className="dropdown-item" href="/Login">Login</a></li>
+                      <li><a className="dropdown-item" href="/Register">Register</a></li>
+                    </React.Fragment>
+                  )}
+                  {isUser && <li><a className="dropdown-item" href="/Settings">Settings</a></li>}
                   <hr className="dropdown-divider"/>
-                  <li><a className="dropdown-item" onClick = {logout}>Logout</a></li>
+                  <li><a className="dropdown-item" onClick={logout}>Logout</a></li>
                 </ul>
-                
-                <div>
-                  {isUser == true ? "Signed in as " + user : "Not signed in"}
-                </div>
               </li>
             </ul>
-            
           </div>
         </div>
       </div>
