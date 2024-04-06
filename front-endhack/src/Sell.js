56b4c93b6a92f2d90f509cd9469684ea3e4812b5
Rishit Gupta <57046026+Rashod-coder@users.noreply.github.com>
2024-04-05 22:44:47 -0700

    changes made

--- front-endhack/src/Sell.js
+++ front-endhack/src/Sell.js
@@ -1,11 +1,11 @@
-import {useState} from 'react'
+import { useState } from 'react';
 import { collection, addDoc } from "firebase/firestore"; 
+import { db } from "./Firestore/Firestore";
+import './Sell.css'; // Import your custom CSS file for Sell component styling
 
-import {db} from "./Firestore/Firestore"
-function Sell(){
-    //lets get data
+function Sell() {
     const [desc, setDesc] = useState("");
-    const [rate, setRate] = useState(.01);
+    const [rate, setRate] = useState(0.01);
     const [maxAmt, setMaxAmt] = useState(1);
     const [minAmt, setMinAmt] = useState(0);
     const [type, setType] = useState("");
@@ -13,68 +13,59 @@ function Sell(){
     const [street, setStreet] = useState("");
     const [city, setCity] = useState("");
     const [zipcode, setZipcode] = useState("");
-    const keepDatabase = async() => {
+
+    const keepDatabase = async () => {
         try {
             const docRef = await addDoc(collection(db, "Orders"), {
-              Street: street,
-              maxAmount: maxAmt,
-              minAmount: minAmt,
-              Price: rate,
-              City: city,
-              Zipcode: zipcode,
-              Type: type
+                Street: street,
+                maxAmount: maxAmt,
+                minAmount: minAmt,
+                Price: rate,
+                City: city,
+                Zipcode: zipcode,
+                Type: type
             });
             console.log("Document written with ID: ", docRef.id);
-          } catch (e) {
+        } catch (e) {
             console.error("Error adding document: ", e);
-          }    }
+        }
+    }
 
-    return(
-        <div id = "box"className='wrapper'>
-            <div className='input-box'>
-            <input type="text" placeholder='Type of produce (e.x: Apple)' name="type" onChange={(event) => {
-                setType(event.target.value);
-            }}/>
+    return (
+        <div className="container-fluid sell-container">
+            <div className="row">
+             <div className="col-lg-6">
+                <form className="sell-form">
+                        <div className="form-group">
+                            <input type="text" className="form-control" placeholder="Type of produce (e.g., Apple)" name="type" onChange={(event) => setType(event.target.value)} />
+                        </div>
+                        <div className="form-group">
+                            <input type="text" className="form-control" placeholder="Rate per pound" name="rate" onChange={(event) => setRate(event.target.value)} />
+                        </div>
+                        <div className="form-group">
+                            <input type="text" className="form-control" placeholder="Amount of produce (e.g., 5 pounds)" name="maxAmt" onChange={(event) => setMaxAmt(event.target.value)} />
+                        </div>
+                        <div className="form-group">
+                            <input type="text" className="form-control" placeholder="Minimum amount per purchase" name="minAmt" onChange={(event) => setMinAmt(event.target.value)} />
+                        </div>
+                        <div className="form-group">
+                            <input type="text" className="form-control" placeholder="Description of product" name="desc" onChange={(event) => setDesc(event.target.value)} />
+                        </div>
+                        <div className="form-group">
+                            <input type="text" className="form-control" placeholder="Street" name="street" onChange={(event) => setStreet(event.target.value)} />
+                        </div>
+                        <div className="form-group">
+                            <input type="text" className="form-control" placeholder="City" name="city" onChange={(event) => setCity(event.target.value)} />
+                        </div>
+                        <div className="form-group">
+                            <input type="text" className="form-control" placeholder="Zipcode" name="zipcode" onChange={(event) => setZipcode(event.target.value)} />
+                        </div>
+                        <button type="button" className="btn btn-primary btn-block" onClick={keepDatabase}>Upload</button>
+                    </form>
+                </div>
             </div>
-            <div className='input-box'>
-            <input type="text" placeholder='Rate per pound' name="rate" onChange={(event) => {
-                setRate(event.target.value);
-            }}/>
-            </div>
-            <div className='input-box'>
-            <input type="text" placeholder='Amount of produce (e.x: 5 pounds)' name="mxamt" onChange={(event) => {
-                setMaxAmt(event.target.value);
-            }}/>
-            </div>
-            <div className='input-box'>
-            <input type="text" placeholder='Minimum amount per purchase' name="mnamt"  onChange={(event) => {
-                setMinAmt(event.target.value);
-            }}/>
-            </div>
-            <div className='input-box'>
-            <input type="text" placeholder='Description of product' name="desc"  onChange={(event) => {
-                setDesc(event.target.value);
-            }}/>
-            </div>
-
-
-            <div className='input-box'>
-            <input type="text" placeholder='Street' name="street"  onChange={(event) => {
-                setStreet(event.target.value);
-            }}/>
-            </div>
-            <div className='input-box'>
-            <input type="text" placeholder='City' name="city"  onChange={(event) => {
-                setCity(event.target.value);
-            }}/>
-            </div>
-            <div className='input-box'>
-            <input type="text" placeholder='Zipcode' name="zipcode"  onChange={(event) => {
-                setZipcode(event.target.value);
-            }}/>
-            </div>
-            <button onClick = {keepDatabase}>Upload</button>
         </div>
     );
 }
+
 export default Sell;
