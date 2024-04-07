56f422b19edf0faf93d87ed458075306b5d9acec
Nikhil Pellakuru <76831568+nikrp@users.noreply.github.com>
2024-04-07 09:57:35 -0700

    Dashboard done

--- front-endhack/src/secondarySrc/components/ProductsAnalytics.js
+++ front-endhack/src/secondarySrc/components/ProductsAnalytics.js
@@ -5,27 +5,80 @@ import { getDoc, doc, collection, where, query, getDocs } from 'firebase/firesto
 import { db } from "../../Firestore/Firestore";
 import { auth } from "../../auth/Authentication";
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { TbCalendarDollar } from "react-icons/tb";
+import { IoAnalyticsSharp } from "react-icons/io5";
+import { PieChart, Pie, Sector, Cell } from 'recharts';
 
-export default function ProductsAnalytics() {
+const data2 = [
+  { name: 'Group A', value: 400 },
+  { name: 'Group B', value: 300 },
+  { name: 'Group C', value: 300 },
+  { name: 'Group D', value: 200 },
+];
 
-    const [totalEarnings, setTotalEarnings] = useState(undefined);
-    const [totalSales, setTotalSales] = useState(undefined);
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
-    useEffect(() => {
-        const getAnalytics = async () => {
-            const usersDoc = collection(db, "users");
-            const q = query(usersDoc, where("email", "==", auth.currentUser.email));
-            const snapshot = await getDocs(q);
-            const earnings = snapshot.docs[0].data()["earnings"];
-            const sales = snapshot.docs[0].data()["sales"];
-            setTotalEarnings(Math.round(parseFloat(earnings) * 100) / 100);
-            setTotalSales(parseInt(sales));
-        }
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
-        getAnalytics();
-    }, []);
+  return (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
 
-    const data = [
+
+const data = [
+    {
+        name: 'Monday',
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+      },
+      {
+        name: 'Tuesday',
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+      },
+      {
+        name: 'Wednesday',
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+      },
+      {
+        name: 'Thursday',
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+      },
+      {
+        name: 'Friday',
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+      },
+      {
+        name: 'Saturday',
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+      },
+      {
+        name: 'Sunday',
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+      },
+]
+
+const datasets = {
+    "Week": [
         {
             name: 'Monday',
             uv: 4000,
@@ -68,7 +121,139 @@ export default function ProductsAnalytics() {
             pv: 4300,
             amt: 2100,
           },
-    ]
+    ],
+    "Month": [
+            {
+                name: 'Jan',
+                uv: 4000,
+                pv: 2400,
+                amt: 2400,
+              },
+              {
+                name: 'Feb',
+                uv: 3000,
+                pv: 1398,
+                amt: 2210,
+              },
+              {
+                name: 'March',
+                uv: 2000,
+                pv: 9800,
+                amt: 2290,
+              },
+              {
+                name: 'April',
+                uv: 2780,
+                pv: 3908,
+                amt: 2000,
+              },
+              {
+                name: 'May',
+                uv: 1890,
+                pv: 4800,
+                amt: 2181,
+              },
+              {
+                name: 'June',
+                uv: 2390,
+                pv: 3800,
+                amt: 2500,
+              },
+              {
+                name: 'July',
+                uv: 3490,
+                pv: 4300,
+                amt: 2100,
+              },
+              {
+                name: 'Aug',
+                uv: 1890,
+                pv: 4800,
+                amt: 2181,
+              },
+              {
+                name: 'Sep',
+                uv: 2390,
+                pv: 3800,
+                amt: 2500,
+              },
+              {
+                name: 'Oct',
+                uv: 3490,
+                pv: 4300,
+                amt: 2100,
+              },
+              {
+                name: 'Nov',
+                uv: 1890,
+                pv: 4800,
+                amt: 2181,
+              },
+              {
+                name: 'Dec',
+                uv: 2390,
+                pv: 3800,
+                amt: 2500,
+              },
+    ],
+    "Year": data,
+    "YTD": data,
+    "All Time": data,
+}
+
+const orders = [
+    {
+        name: "Katherine Smith",
+        product: "Avacados",
+        pounds: 12,
+        cost: 11.47,
+    },
+    {
+        name: "John Doe",
+        product: "Bananas",
+        pounds: 2,
+        cost: 1.39,
+    },
+    {
+        name: "Jane Foster",
+        product: "Mangos",
+        pounds: 10,
+        cost: 12.52,
+    },
+    {
+        name: "Alex Leonardo",
+        product: "Grapefruit",
+        pounds: 6.47,
+        cost: 6.23,
+    }
+]
+
+export default function ProductsAnalytics() {
+
+    const [totalEarnings, setTotalEarnings] = useState(undefined);
+    const [totalSales, setTotalSales] = useState(undefined);
+    const [choiceIndex, setChoiceIndex] = useState(0);
+    const choices = ["Week", "Month", "Year", "YTD", "All Time"];
+    const [currentDataset, setCurrentDataset] = useState(datasets[choices[choiceIndex]])
+
+    useEffect(() => {
+        const getAnalytics = async () => {
+            const usersDoc = collection(db, "users");
+            const q = query(usersDoc, where("email", "==", auth.currentUser.email));
+            const snapshot = await getDocs(q);
+            const earnings = snapshot.docs[0].data()["earnings"];
+            const sales = snapshot.docs[0].data()["sales"];
+            console.log(sales)
+            setTotalEarnings(Math.round(parseFloat(earnings) * 100) / 100);
+            setTotalSales(parseInt(sales));
+        }
+
+        getAnalytics();
+    }, []);
+
+    useEffect(() => {
+        setCurrentDataset(datasets[choices[choiceIndex]]);
+    }, [choiceIndex]);
 
     return (
         <div className={`h-[91%]`}>
@@ -77,42 +262,52 @@ export default function ProductsAnalytics() {
                     <div className={`bg-gray-200 rounded-full w-fit p-3 my-2 mr-3`}><FaChartSimple size={30} className={`fill-purple-700`} /></div>
                     <div>
                         <p className={`text-md text-gray-400 font-medium`}>Total Earnings</p>
-                        <p className={`text-xl text-black font-semibold`}>{totalEarnings ? "$" + totalEarnings : "---"}</p>
+                        <p className={`text-xl text-black font-semibold`}>{totalEarnings ? "$" + totalEarnings : 0}</p>
                     </div>
                 </div>
                 <div className={`w-[24%] h-fit px-3 py-2 rounded-lg bg-white flex flex-row items-center`}>
-                    <div className={`bg-gray-200 rounded-full w-fit p-3 my-2 mr-3`}><FaCalendar size={30} className={`fill-purple-700`} /></div>
+                    <div className={`bg-gray-200 rounded-full w-fit p-3 my-2 mr-3`}><TbCalendarDollar size={30} className={`text-purple-700`} /></div>
                     <div>
                         <p className={`text-md text-gray-400 font-medium`}>Earnings this Month</p>
-                        <p className={`text-xl text-black font-semibold`}>{totalEarnings / 3 ? "$" + (Math.round((totalEarnings / 3) * 100) / 100) : "---"}</p>
+                        <p className={`text-xl text-black font-semibold`}>{totalEarnings / 3 ? "$" + (Math.round((totalEarnings / 3) * 100) / 100) : 0}</p>
                     </div>
                 </div>
                 <div className={`w-[24%] h-fit px-3 py-2 rounded-lg bg-white flex flex-row items-center`}>
-                    <div className={`bg-gray-200 rounded-full w-fit p-3 my-2 mr-3`}><FaCalendar size={30} className={`fill-purple-700`} /></div>
+                    <div className={`bg-gray-200 rounded-full w-fit p-3 my-2 mr-3`}><IoAnalyticsSharp size={30} className={`fill-purple-700`} /></div>
                     <div>
                         <p className={`text-md text-gray-400 font-medium`}>Total Sales</p>
-                        <p className={`text-xl text-black font-semibold`}>{totalSales ? (parseInt((totalSales))) : "---"}</p>
+                        <p className={`text-xl text-black font-semibold`}>{totalSales ? (parseInt((totalSales))) : 0}</p>
                     </div>
                 </div>
                 <div className={`w-[24%] h-fit px-3 py-2 rounded-lg bg-white flex flex-row items-center`}>
                     <div className={`bg-gray-200 rounded-full w-fit p-3 my-2 mr-3`}><FaCalendar size={30} className={`fill-purple-700`} /></div>
                     <div>
                         <p className={`text-md text-gray-400 font-medium`}>Sales this Month</p>
-                        <p className={`text-xl text-black font-semibold`}>{totalSales - 1 ? (parseInt(totalSales - 1)) : "---"}</p>
+                        <p className={`text-xl text-black font-semibold`}>{totalSales - 1 ? (parseInt(totalSales - 1)) : 0}</p>
                     </div>
                 </div>
             </div>
-            <div className={`w-full flex flex-row items-center justify-between`}>
-                <div className={`w-[65%] bg-white rounded-xl py-5 pt-6`}>
+            <div className={`w-full flex flex-row justify-between`}>
+                <div className={`w-[65%] bg-white rounded-xl py-3 px-10 h-full`}>
+                    <div className={`flex flex-row items-center mb-5 w-5/6 mx-auto`}>
+                        <h3 className={`mr-auto text-2xl font-semibold`}>Earnings</h3>
+                        <button className={`bg-gray-300 text-black font-semibold px-3 py-1 rounded-lg hover:bg-gray-200 w-fit h-fit`} onClick={() => {
+                            if (choiceIndex === choices.length - 1) {
+                                setChoiceIndex(0);
+                            } else {
+                                setChoiceIndex(index => index += 1);
+                            }
+                        }}>{choices[choiceIndex]}</button>
+                    </div>
                         <LineChart
                         className={`mx-auto`}
                         width={800}
                         height={300}
-                        data={data}
+                        data={currentDataset}
                         margin={{
                             top: 5,
                             right: 30,
-                            left: 20,
+                            left: 10,
                             bottom: 5,
                         }}
                         >
@@ -125,7 +320,19 @@ export default function ProductsAnalytics() {
                         <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
                         </LineChart>
                 </div>
-                <div className={`w-[32.5%] bg-white rounded-xl h-full`}></div>
+                <div className={`w-[33.35%] bg-white rounded-xl py-2 px-10 h-full`}>
+                    <h1 className={`text-center text-2xl font-semibold mb-3`}>Orders</h1>
+                    <div className={`w-full`}>
+                        {orders.map((order, index) => {
+                            return (
+                                <div key={index} className={`w-full border-b-2 border-b-gray-400 mb-3`}>
+                                    <p className={`w-full text-lg flex justify-between`}><span className={`mr-auto font-semibold text-lg`}>{order.name}</span><span className={`ml-auto text-green-600 font-medium`}>${order.cost}</span></p>
+                                    <p className={`w-full text-md flex justify-between mb-3`}><span className={`mr-auto font-medium text-md text-gray-500`}>{order.product}</span><span className={`text-gray-500 font-medium`}>{order.pounds} lbs</span></p>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
             </div>
         </div>
     )
--- front-endhack/src/secondarySrc/pages/Home.js
+++ front-endhack/src/secondarySrc/pages/Home.js
@@ -7,7 +7,12 @@ import { IoClose } from 'react-icons/io5';
 import { FaHome } from "react-icons/fa";
 import { SiWheniwork } from "react-icons/si";
 import { GoGoal } from "react-icons/go";
-import { IoCall } from "react-icons/io5";
+import { IoCall, IoSparkles } from "react-icons/io5";
+import oauthImage from '../assets/oauth.jpg';
+import cardLayout from '../assets/card-layout.png';
+import payPalProof from '../assets/paypal-proof.jpg';
+import uploadSelling from '../assets/uploads.png';
+import dashboardImage from '../assets/dashboard.png';
 
 export default function Home() {
     const videoRef = useRef();
@@ -28,18 +33,18 @@ export default function Home() {
                 {!navOpen && <div className={`z-50 fixed *:sm:hidden top-2 left-2 rounded-lg hover:bg-gray-500 hover:bg-opacity-75`}>
                     <RxHamburgerMenu className={`block fill-white text-white cursor-pointer m-1`} size={40} onClick={() => setNavOpen(true)} />
                 </div>}
-                <div className={`h-[14%] z-50 flex flex-row items-center justify-between w-4/6 mx-auto hidden sm:flex`}>
+                <div className={`h-[14%] z-50 flex flex-row items-center justify-between w-5/6 mx-auto hidden sm:flex`}>
                     <h2 className={`font-bold text-5xl mr-auto text-white flex`}>Fresh for All</h2>
                     <div className={`flex flex-row items-center mr-auto`}>
-                        <p className={`mr-10 font-bold text-xl text-white`}>Home</p>
-                        <p className={`mr-10 font-bold text-xl text-white`}>Features</p>
-                        <p className={`mr-10 font-bold text-xl text-white`}>Our Mission</p>
-                        <p className={`mr-10 font-bold text-xl text-white`}>Contact</p>
+                        <a className={`mr-10 font-bold text-xl text-white cursor-pointer`} href='#'>Home</a>
+                        <a className={`mr-10 font-bold text-xl text-white`} href='#features'>Features</a>
+                        <a className={`mr-10 font-bold text-xl text-white`} href='#mission'>Our Mission</a>
+                        <a className={`mr-10 font-bold text-xl text-white`} href='#contact'>Contact</a>
                     </div>
                     {!auth.currentUser ? (
                         <div className={`flex flex-row items-center`}>
-                            <button onClick={() => navigate('/Login')} className={`px-3 mr-10 py-1 rounded-lg bg-purple-700 border-purple-700 border-2 border-spacing-8 hover:bg-purple-600 hover:border-purple-600 transition-all duration-200 ease-in-out text-lg text-white font-semibold`}>Sign In</button>
-                            <button onClick={() => navigate('/Register')} className={`px-3 py-1 rounded-lg bg-purple-700 border-2 border-purple-700 hover:bg-purple-600 hover:border-purple-600 transition-all duration-200 ease-in-out text-lg text-white font-semibold`}>Register</button>
+                            <button onClick={() => navigate('/Login')} className={`px-3 mr-10 w-fit h-fit py-1 rounded-lg border-purple-700 bg-none border-2 border-spacing-8 hover:bg-purple-600 hover:border-purple-600 transition-all duration-200 ease-in-out text-lg text-white font-semibold`}>Sign In</button>
+                            <button onClick={() => navigate('/Register')} className={`px-3 py-1 w-fit h-fit rounded-lg bg-purple-700 border-2 border-purple-700 hover:bg-purple-600 hover:border-purple-600 transition-all duration-200 ease-in-out text-lg text-white font-semibold`}>Register</button>
                         </div>
                     ) : (
                         <button onClick={() => navigate('/Dashboard')} className={` w-fit px-3 py-1 rounded-lg border-purple-700 bg-purple-700 mr-5 border-2 border-spacing-8 hover:bg-purple-600 hover:border-purple-600 transition-all duration-200 ease-in-out text-lg text-white font-semibold`}>Dashboard</button>
@@ -52,61 +57,66 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-            <div className={`h-screen bg-white w-screen px-20 py-10`}>
+            <div id='features' className={`h-screen bg-white w-screen px-20 py-10`}>
                 <h2 className={`text-4xl font-semibold mb-4`}>Features</h2>
                 <div className={`flex flex-wrap justify-between`}>
                     <div className={`shadow-lg bg-white rounded-xl w-[32%] mb-4`}>
-                        <div className={`bg-gray-200 h-48 rounded-t-xl`}></div>
-                        <div className={`h-36 bg-white shadow-inner rounded-b-xl px-4 py-3`}>
+                        <div className={`bg-gray-200 h-48 rounded-t-xl flex justify-center items-center`}>
+                            <IoSparkles size={150} fill='gold' />
+                        </div>
+                        <div className={`h-40 bg-white shadow-inner rounded-b-xl px-4 py-3`}>
                             <p className={`text-xl font-semibold mb-1`}>ML Trained Model </p>
                             <p className={`text-md`}>Our trained ML model will suggest an accurate price of the product, based on the current Economy,  you are currently trying to sell</p>
                         </div>
                     </div>
                     <div className={`shadow-lg bg-white rounded-xl w-[32%] mb-4`}>
-                        <div className={`bg-gray-200 h-48 rounded-t-xl`}></div>
-                        <div className={`h-36 bg-white shadow-inner rounded-b-xl px-4 py-3`}>
-                            <p className={`text-xl font-semibold mb-1`}>Feature 1</p>
-                            <p className={`text-md`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Iaculis nunc sed augue lacus viverra.</p>
+                        {/* <div className={`bg-gray-200 h-48 rounded-t-xl`}></div> */}
+                        <img src={oauthImage} alt='Sign In With Google Feature' className={`h-48 rounded-t-xl shadow-md`} />
+                        <div className={`h-40 bg-white shadow-inner rounded-b-xl px-4 py-3`}>
+                            <p className={`text-xl font-semibold mb-1`}>Google OAuth</p>
+                            <p className={`text-md`}>With the powerful features of Google comes OAuth, a revolutionary authentication system providing easy login/signup integrations and a better UI/UX for users around the world.</p>
                         </div>
                     </div>
                     <div className={`shadow-lg bg-white rounded-xl w-[32%] mb-4`}>
-                        <div className={`bg-gray-200 h-48 rounded-t-xl`}></div>
-                        <div className={`h-36 bg-white shadow-inner rounded-b-xl px-4 py-3`}>
-                            <p className={`text-xl font-semibold mb-1`}>Feature 1</p>
-                            <p className={`text-md`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Iaculis nunc sed augue lacus viverra.</p>
+                        {/* <div className={`bg-gray-200 h-48 rounded-t-xl`}></div> */}
+                        <img src={cardLayout} alt='Sign In With Google Feature' className={`h-48 rounded-t-xl shadow-md w-[81%] mx-auto`} />
+                        <div className={`h-40 bg-white shadow-inner rounded-b-xl px-4 py-3`}>
+                            <p className={`text-xl font-semibold mb-1`}>Readable Data</p>
+                            <p className={`text-md`}>Using modern styling tools such as Bootstrap and TailwindCSS, we created easy understandable cards which each contain information for reading or preview purposes.</p>
                         </div>
                     </div>
                     <div className={`shadow-lg bg-white rounded-xl w-[32%]`}>
-                        <div className={`bg-gray-200 h-48 rounded-t-xl`}></div>
-                        <div className={`h-36 bg-white shadow-inner rounded-b-xl px-4 py-3`}>
-                            <p className={`text-xl font-semibold mb-1`}>Feature 1</p>
-                            <p className={`text-md`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Iaculis nunc sed augue lacus viverra.</p>
+                        {/* <div className={`bg-gray-200 h-48 rounded-t-xl`}></div> */}
+                        <img src={payPalProof} alt='Sign In With Google Feature' className={`h-48 rounded-t-xl shadow-md w-full object-cover object-top overflow-hidden mx-auto`} />
+                        <div className={`h-40 bg-white shadow-inner rounded-b-xl px-4 py-3`}>
+                            <p className={`text-xl font-semibold mb-1`}>Paypal Integration</p>
+                            <p className={`text-md`}>Paypal makes integrating payments into our application twice as easy. Quick components already provided by Paypals developer application allowed us to easily connect users together and focus more on looks.</p>
                         </div>
                     </div>
                     <div className={`shadow-lg bg-white rounded-xl w-[32%]`}>
-                        <div className={`bg-gray-200 h-48 rounded-t-xl`}></div>
-                        <div className={`h-36 bg-white shadow-inner rounded-b-xl px-4 py-3`}>
-                            <p className={`text-xl font-semibold mb-1`}>Feature 1</p>
-                            <p className={`text-md`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Iaculis nunc sed augue lacus viverra.</p>
+                        {/* <div className={`bg-gray-200 h-48 rounded-t-xl`}></div> */}
+                        <img src={uploadSelling} alt='Sign In With Google Feature' className={`h-48 rounded-t-xl shadow-md w-full object-cover object-top overflow-hidden mx-auto`} />
+                        <div className={`h-40 bg-white shadow-inner rounded-b-xl px-4 py-3`}>
+                            <p className={`text-xl font-semibold mb-1`}>AI Price Sugestion</p>
+                            <p className={`text-md`}>Along with a seamless product uploads page, vendors can find a price with the click of  button through our application. Using an unsupervised model trained on a dataset of prices and produce, we predict the prices of produce the user has entered.</p>
                         </div>
                     </div>
                     <div className={`shadow-lg bg-white rounded-xl w-[32%]`}>
-                        <div className={`bg-gray-200 h-48 rounded-t-xl`}></div>
-                        <div className={`h-36 bg-white shadow-inner rounded-b-xl px-4 py-3`}>
-                            <p className={`text-xl font-semibold mb-1`}>Feature 1</p>
-                            <p className={`text-md`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Iaculis nunc sed augue lacus viverra.</p>
+                        {/* <div className={`bg-gray-200 h-48 rounded-t-xl`}></div> */}
+                        <img src={dashboardImage} alt='Sign In With Google Feature' className={`h-48 rounded-t-xl shadow-md w-full object-cover object-top overflow-hidden mx-auto`} />
+                        <div className={`h-40 bg-white shadow-inner rounded-b-xl px-4 py-3`}>
+                            <p className={`text-xl font-semibold mb-1`}>Informational Analytics</p>
+                            <p className={`text-md`}>Our user's built-in dashboard page allows them to see analytics on what they're selling such as profits and sales. Also available are lists and charts on orders and their prices.</p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className={`h-fit w-screen bg-gray-800 px-20 py-10`}>
+            <div id='mission' className={`h-fit w-screen bg-gray-800 px-20 py-10`}>
                 <h2 className={`text-4xl font-semibold mb-4 text-white`}>Our Mission</h2>
                 <p className={`text-2xl text-white leading-relaxed font-normal`}>Our Mission is to provide Fresh Produce for all. We notice that many people grow produce in their garden however many of these go to waste or end up the ground, so we implemented Fresh For All where people can upload their produce online and people </p>
             </div>
-            <div className={`h-fit w-screen bg-white px-20 py-10`}>
+            <div id='contact' className={`h-fit w-screen bg-white px-20 py-10`}>
                 <h2 className={`text-4xl font-semibold mb-4 text-black`}>Contact</h2>
-                <h3>Instagram</h3>
-                <h3>Download our App for Mobiles</h3>
                 <div className={`w-2/6 mx-auto`}>  
                     <div className={`w-full flex justify-between mb-3`}>
                         <input className={`px-3 w-[49%] py-1 text-lg border-b-2 focus:outline-none border-gray-400 focus:border-purple-600`} placeholder='Enter your name'/>
@@ -115,6 +125,9 @@ export default function Home() {
                     <textarea className={`text-lg mb-3 border-gray-400 rounded-xl focus:outline-none border-2 px-3 py-2 w-full focus:border-purple-600`} placeholder="Enter your message and we'll get back to you as soon as possible!" rows={3}></textarea>
                     <button className={`w-full bg-purple-700 hover:bg-purple-600 transition-all duration-200 ease-in-out py-2 px-3 text-lg text-white`}>Send Message</button>
                 </div>
+            </div>
+            <div id='footer' className={`h-fit w-screen bg-gray-800 px-20 py-10`}>
+                <h1 className={`w-full text-center text-3xl font-bold text-green-500 tracking-wider`}>&copy; Fresh for All</h1>
             </div>
             {navOpen && (
                 <div className={'w-screen bg-gray-800 fixed top-0 left-0 h-screen px-3 py-3'}>
