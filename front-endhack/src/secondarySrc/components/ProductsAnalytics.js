3786f8b9022cc0f170e2aaca19dd2232c1e0db2f
Nikhil Pellakuru <76831568+nikrp@users.noreply.github.com>
2024-04-07 07:50:35 -0700

    Added hardcoded graph

--- front-endhack/src/secondarySrc/components/ProductsAnalytics.js
+++ front-endhack/src/secondarySrc/components/ProductsAnalytics.js
@@ -25,37 +25,107 @@ export default function ProductsAnalytics() {
         getAnalytics();
     }, []);
 
+    const data = [
+        {
+            name: 'Monday',
+            uv: 4000,
+            pv: 2400,
+            amt: 2400,
+          },
+          {
+            name: 'Tuesday',
+            uv: 3000,
+            pv: 1398,
+            amt: 2210,
+          },
+          {
+            name: 'Wednesday',
+            uv: 2000,
+            pv: 9800,
+            amt: 2290,
+          },
+          {
+            name: 'Thursday',
+            uv: 2780,
+            pv: 3908,
+            amt: 2000,
+          },
+          {
+            name: 'Friday',
+            uv: 1890,
+            pv: 4800,
+            amt: 2181,
+          },
+          {
+            name: 'Saturday',
+            uv: 2390,
+            pv: 3800,
+            amt: 2500,
+          },
+          {
+            name: 'Sunday',
+            uv: 3490,
+            pv: 4300,
+            amt: 2100,
+          },
+    ]
+
     return (
         <div className={`h-[91%]`}>
-            <div className={`w-full flex flex-row justify-between`}>
-                <div className={`w-[24.5%] h-fit px-3 py-2 rounded-lg bg-white flex flex-row items-center`}>
+            <div className={`w-full flex flex-row justify-between mb-3`}>
+                <div className={`w-[24%] h-fit px-3 py-2 rounded-lg bg-white flex flex-row items-center`}>
                     <div className={`bg-gray-200 rounded-full w-fit p-3 my-2 mr-3`}><FaChartSimple size={30} className={`fill-purple-700`} /></div>
                     <div>
                         <p className={`text-md text-gray-400 font-medium`}>Total Earnings</p>
                         <p className={`text-xl text-black font-semibold`}>{totalEarnings ? "$" + totalEarnings : "---"}</p>
                     </div>
                 </div>
-                <div className={`w-[24.5%] h-fit px-3 py-2 rounded-lg bg-white flex flex-row items-center`}>
+                <div className={`w-[24%] h-fit px-3 py-2 rounded-lg bg-white flex flex-row items-center`}>
                     <div className={`bg-gray-200 rounded-full w-fit p-3 my-2 mr-3`}><FaCalendar size={30} className={`fill-purple-700`} /></div>
                     <div>
                         <p className={`text-md text-gray-400 font-medium`}>Earnings this Month</p>
                         <p className={`text-xl text-black font-semibold`}>{totalEarnings / 3 ? "$" + (Math.round((totalEarnings / 3) * 100) / 100) : "---"}</p>
                     </div>
                 </div>
-                <div className={`w-[24.5%] h-fit px-3 py-2 rounded-lg bg-white flex flex-row items-center`}>
+                <div className={`w-[24%] h-fit px-3 py-2 rounded-lg bg-white flex flex-row items-center`}>
                     <div className={`bg-gray-200 rounded-full w-fit p-3 my-2 mr-3`}><FaCalendar size={30} className={`fill-purple-700`} /></div>
                     <div>
                         <p className={`text-md text-gray-400 font-medium`}>Total Sales</p>
                         <p className={`text-xl text-black font-semibold`}>{totalSales ? (parseInt((totalSales))) : "---"}</p>
                     </div>
                 </div>
-                <div className={`w-[24.5%] h-fit px-3 py-2 rounded-lg bg-white flex flex-row items-center`}>
+                <div className={`w-[24%] h-fit px-3 py-2 rounded-lg bg-white flex flex-row items-center`}>
                     <div className={`bg-gray-200 rounded-full w-fit p-3 my-2 mr-3`}><FaCalendar size={30} className={`fill-purple-700`} /></div>
                     <div>
                         <p className={`text-md text-gray-400 font-medium`}>Sales this Month</p>
                         <p className={`text-xl text-black font-semibold`}>{totalSales - 1 ? (parseInt(totalSales - 1)) : "---"}</p>
                     </div>
                 </div>
+            </div>
+            <div className={`w-full flex flex-row items-center justify-between`}>
+                <div className={`w-[65%] bg-white rounded-xl py-5 pt-6`}>
+                        <LineChart
+                        className={`mx-auto`}
+                        width={800}
+                        height={300}
+                        data={data}
+                        margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                        >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+                        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                        </LineChart>
+                </div>
+                <div className={`w-[32.5%] bg-white rounded-xl h-full`}></div>
             </div>
         </div>
     )
